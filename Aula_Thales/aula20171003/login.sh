a5fb3e227f8e573b1df2700d09f18236cf167138
root <viniciusxbox2014@hotmail.com>
2017-10-04 16:51:04 -0300

    Login com dialog

--- Aula_Thales/aula20171003/login.sh
+++ Aula_Thales/aula20171003/login.sh
@@ -9,11 +9,22 @@ senha="0ed5a4f0a1bed1b93113d77361b90b202cf85985e4f9d59ba4ff404a0100dc11"
 CRIAR(){
 
 	#read -p "Criando usuário"
-	read -p "Digite o nome do usuário a ser criado: " CRIARUSER
+
+
+	#read -p "Digite o nome do usuário a ser criado: " CRIARUSER
+
+	CRIARUSER=$(dialog			\
+		    --stdout			\
+		    --title "Criar usuário"	\
+		    --inputbox "Digite o nome do usuário a ser criado: "	\
+		    0 0)
+
 	useradd -m -s /bin/tcsh $CRIARUSER #-m Cria o home/
 	
 	passwd $CRIARUSER
 	
+	
+		
 	
 	read -p "Criado com sucesso. Pressione [enter] para finalizar."
 	menu
@@ -37,8 +48,14 @@ menu(){
 	echo "1) Criar usuários"
 	echo "2) Remover usuários"
 	echo "3) Sair"
-	read -p "Escolha uma opção: " OPCAO 
+	#read -p "Escolha uma opção: " OPCAO 
 	#-p vai chamar o prompt para a entrada de dados pelo usuário
+
+	OPCAO=$( dialog				  \
+			 --stdout			  \
+			 --title "Opções" 		  \
+			 --inputbox "Digite uma das opções: " \
+			 0 0)	
 
 	
 	case $OPCAO in
@@ -58,8 +75,23 @@ logar(){
 	clear
 	echo
 
-	read -p "Usuário: " USER
-	read -s -p "Senha: " PASS #Seta a variável PASS
+	#read -p "Usuário: " USER
+
+	USER=$( dialog					\
+		--stdout				\
+		--title "Usuário"			\
+		--inputbox "Digite o nome do usuário: " \
+		0 0)
+		
+	PASS=$( dialog					\
+		--stdout				\
+		--title "Senha"				\
+		--passwordbox "Digite sua senha: " 	\
+		0 0)
+	
+
+	#read -s -p "Senha: " PASS #Seta a variável PASS
+
 	PASS=$(echo $PASS | sha256sum | cut -d" " -f1) #Passa o $PASS para o hash
 [ $USER == $usuario ] && [ $PASS == $senha ] && menu || logar
 
